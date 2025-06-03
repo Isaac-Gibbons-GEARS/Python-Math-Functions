2a3fc681ff1c608683af3d30b37084a89165dc53
Isaac-Gibbons <isaac7922@outlook.com>
2025-06-03 08:54:33 -0700

    Add files via upload

---
+++ sum of-sqare math function.py
@@ -0,0 +1,13 @@
+def sumrangesqr(xmin, xmax, base, added):
+    """ The components of the sum-of math function. Xmin is the
+    starting value, xmax is the maximum x value, base is the value x is multiplied by,
+    and added is any value added to x. If there is no multiplier for x, type 1 for base,
+    and if there is no number that is added to x, type 0 for added."""   
+    total = 0  # Set up the total variable
+    numberlist = list(range(xmin, xmax + 1))  # Calculates the list of values for x
+    times_to_repeat = (xmax-xmin)+1 #calculates how many times to run the for loop.
+    for x in range(times_to_repeat):
+        number = numberlist[x] #gets each value of x.
+        result = (base**number)+added #calculates the result with the x value.
+        total = total + result #collects the results.
+    return total
